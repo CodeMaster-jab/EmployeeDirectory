829183207d7e9816360c9d268050db48a94ed78b
Code Master <jeffbell.kc@gmail.com>
2020-04-18 23:04:41 -0500

    Polished code

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,8 +3,27 @@ import React from "react";
 function SearchForm(props) {
   return (
     <form>
-      
       <div className="form-group">
+        <label htmlFor="sortby" >Sort Order:</label>
+        <input 
+          className="separate"
+          onChange={props.handleInputChange}
+          type="radio"
+          name="sortby"
+          radioGroup="sortby"
+          id="lastname"
+          checked={(props.sortby === "lastname") ? "checked" : false}
+        />Last Name
+        <input
+          className="separate"
+          onChange={props.handleInputChange}
+          type="radio"
+          name="sortby"
+          radioGroup="sortby"
+          id="firstname"
+          checked={(props.sortby === "firstname") ? "checked" : false}
+        />First Name
+        <br></br>
         <label htmlFor="filter" >Filter:</label>
         <input 
           className="separate"
@@ -33,26 +52,6 @@ function SearchForm(props) {
           id="female"
           checked={(props.search === "&gender=female") ? "checked" : false}
         />Female
-        <br></br>
-        <label htmlFor="sortby" >Sort Order:</label>
-        <input 
-          className="separate"
-          onChange={props.handleInputChange}
-          type="radio"
-          name="sortby"
-          radioGroup="sortby"
-          id="lastname"
-          checked={(props.sortby === "lastname") ? "checked" : false}
-        />Last Name
-        <input
-          className="separate"
-          onChange={props.handleInputChange}
-          type="radio"
-          name="sortby"
-          radioGroup="sortby"
-          id="firstname"
-          checked={(props.sortby === "firstname") ? "checked" : false}
-        />First Name
         <br></br>
         <button onClick={props.handleFormSubmit} className="btn btn-primary mt-3" >
           Search
